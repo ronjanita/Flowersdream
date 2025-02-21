9c830675e3de23d5270ad94abcc8097a28fbf82d
ronjanita <ronja.nilsson@autismuslink.ch>
2025-02-21 15:42:41 +0100

    basic code structur

--- Flowersdream/Flowersdream/Program.cs
+++ Flowersdream/Flowersdream/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Flowers are still considered pretty even when they dont always bloom the same, look the same or smell the same.");
+        Console.WriteLine("Humans are considered special, weird or ugly as soon as they look different, act different or get old.");
+        Question();
+    }
+
+    static void Question()
+    {
+
+        Console.WriteLine("Would you rather be a flower or a human? (f/h)");
+        Console.ReadLine();
+        string answer = Console.ReadLine();
+        if (answer == "f")
+        {
+            Console.WriteLine("Beeing a flower is for sure really calming.");
+        }
+        else
+        {
+            Console.WriteLine("Beeing a Human must be nice too sometimes.");
+        }
+    }
+    
+}
